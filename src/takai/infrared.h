fa09460738d484ec10052673fd7fea016abe8081
takairyo <ryo@ubuntu.(none)>
2015-11-07 17:03:33 +0900

    赤外線とジャイロ作り始めたわ～

---
+++ src/takai/infrared.h
@@ -0,0 +1,20 @@
+#ifndef INFRARED_H_INCLUDED
+#define INFRARED_H_INCLUDED
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+/*
+*measure_infrared
+*setumei:
+*tyuyuryokuti:型、値の範囲、単位
+*nyuuryokuti:型、引数名、値の範囲、単位
+
+*/
+float measure_infrared(void)
+
+
+#endif
